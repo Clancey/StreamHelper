b1c79041fe07c738ab2aa81534e751eed46e9bc2
James Clancey <james.clancey@gmail.com>
2019-03-29 09:45:29 -0800

    Added mono options, Now uses global settings.

--- StreamHelper/Program.cs
+++ StreamHelper/Program.cs
@@ -5,66 +5,149 @@ using SimpleAuth;
 using System.IO;
 using SimpleAuth.Providers;
 using System.Collections.Generic;
+using Mono.Options;
+using System.Diagnostics;
 
-namespace StreamHelper
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-		
-            Console.WriteLine("Hello World!");
-			args?.ToList ()?.ForEach(Console.WriteLine);
-            Resolver.Register<IAuthStorage, AuthStorage>();
-			var message = "Adding Auto Tweeting to Twitch Markers";
-			SendMessage (message).Wait();
-			UpdateTopic (message);
-			TweetUpdate (message).Wait ();
-			Console.WriteLine ("Done!");
-
-            
-        }
-
-        static async Task  SendMessage(string message)
+namespace StreamHelper {
+	class Program {
+		static async Task Main (string [] args)
 		{
+			Console.WriteLine ("Welcome to the Stream Helper!");
+			Console.WriteLine ($"Args: {String.Join(" ,", args)}");
+
+			Resolver.Register<IAuthStorage, AuthStorage> ();
 			OAuthApi.ShowAuthenticator = async (auth) => {
 
 				var authenticator = new OAuthController (auth);
 				await authenticator.GetCredentials (auth.Title);
 			};
 
-			var api = new TwitchApi ("Twitch", ApiConstants.TwitchApiKey, ApiConstants.TwitchSecret) {
-				Scopes = new [] {
+
+			string message = null;
+			bool show_help = false;
+			bool shouldLogOut = false;
+			var p = new OptionSet () {
+				{ "m|message=", "The Message to be tweeted/Twitched",
+				  v => message = v },
+				{ "p|profile=", "the current profile",
+				  (v) => Settings.Profile = v },
+				{"f|file=","The file path we will update.",v=> Settings.TextFilePath = v },
+				{"logout","log out of all social accounts", v=> shouldLogOut = v != null },
+				{ "h|help",  "show this message and exit",
+				  v => show_help = v != null },
+			};
+
+			List<string> extra;
+			try {
+				extra = p.Parse (args);
+
+			} catch (Exception ex) {
+				Debug.WriteLine (ex);
+				ShowHelp (p);
+				return;
+			}
+			
+			if(show_help || (args?.Length ?? 0) == 0) {
+				ShowHelp (p);
+				return;
+			}
+
+			if(shouldLogOut) {
+				GetTwitchApi ().Logout ();
+				GetTwitterApi ().Logout ();
+			}
+
+			if(string.IsNullOrWhiteSpace(message)) {
+				if (!shouldLogOut)
+					ShowHelp (p);
+				return;
+			}
+
+			await Task.WhenAll (UpdateTopic (message), SetMarkerInTwitch (message), TweetUpdate (message));
+			Console.WriteLine ("Done!");
+
+
+		}
+
+		static void ShowHelp (OptionSet p)
+		{
+			Console.WriteLine ("Usage: greet [OPTIONS]+ message");
+			Console.WriteLine ("Greet a list of individuals with an optional message.");
+			Console.WriteLine ("If no message is specified, a generic greeting is used.");
+			Console.WriteLine ();
+			Console.WriteLine ("Options:");
+			p.WriteOptionDescriptions (Console.Out);
+		}
+
+		static async Task<bool> SetMarkerInTwitch (string message)
+		{
+			var api = GetTwitchApi ();
+			try {
+				var response = await api.UpdateStreamMarker (message);
+				Console.WriteLine (response);
+				return true;
+			} catch (Exception ex) {
+				Console.WriteLine ("Failed to update Twitch");
+				Console.WriteLine (ex);
+				return false;
+			}
+		}
+
+		static async Task<bool> UpdateTopic (string message)
+		{
+			try {
+				var path = Settings.TextFilePath;
+				if (string.IsNullOrWhiteSpace (path)) {
+					Console.WriteLine ("Skipping writing to file, Filepath not set.");
+					return false;
+				}
+				await File.WriteAllTextAsync (path, message);
+				return true;
+			} catch (Exception ex) {
+				Console.WriteLine ("Failed to update Topic Text File");
+				Console.WriteLine (ex);
+				return false;
+			}
+		}
+
+		static async Task<bool> TweetUpdate (string message)
+		{
+			try {
+				var timeSinceTweet = DateTime.Now - Settings.LastTweetTime;
+				if (timeSinceTweet < TimeSpan.FromMinutes(5)) {
+					Console.WriteLine ($"Skipped Tweeting, too soon. Tweeted: {timeSinceTweet}");
+					return false;
+				}
+
+				var api = GetTwitterApi();
+
+				var resp = await api.Post (null, "statuses/update.json", new Dictionary<string, string> {
+					["status"] = $"{message} follow along at twitch.tv/Clancey"
+				});
+				Console.WriteLine ("Tweeting was Successful");
+				Console.WriteLine (resp);
+				return true;
+			}
+			catch(Exception ex) {
+				Console.WriteLine ("Failed to update Twitter");
+				Console.WriteLine (ex);
+				return false;
+			}
+		}
+
+		static TwitterApi twitterApi;
+		static TwitterApi GetTwitterApi () => twitterApi ?? (twitterApi = new TwitterApi (Settings.Profile, ApiConstants.TwitterApiKey, ApiConstants.TwitterSecret) {
+			RedirectUrl = new Uri ("http://localhost"),
+		});
+
+
+		static TwitchApi twitchApi;
+		static TwitchApi GetTwitchApi()=> twitchApi ?? (twitchApi = new TwitchApi (Settings.Profile, ApiConstants.TwitchApiKey, ApiConstants.TwitchSecret) {
+			Scopes = new [] {
 					"clips:edit",
 					"user:edit",
 					"user:edit:broadcast",
 				},
-			};
-			try {
-				var response = await api.UpdateStreamMarker (message);
-				Console.WriteLine (response);
-			}
-			catch(Exception ex) {
-				Console.WriteLine (ex);
-			}
-		}
-
-		static void UpdateTopic(string message)
-		{
-			const string path = "/Users/clancey/Dropbox/LiveStream/CurrentTopic.txt";
-			File.WriteAllText (path, message);
-		}
-
-		static async Task TweetUpdate(string message)
-		{
-			var api = new TwitterApi ("twitter", ApiConstants.TwitterApiKey, ApiConstants.TwitterSecret) {
-				RedirectUrl = new Uri("http://localhost"),
-			};
-
-			var resp = await api.Post (null, "statuses/update.json", new Dictionary<string, string> {
-				["status"] = $"{message} follow along at twitch.tv/Clancey"
-			});
-			Console.WriteLine (resp);
-		}
-    }
+		});
+	}
 }
---
+++ StreamHelper/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Plugin.Settings.Abstractions;
+
+namespace StreamHelper {
+	public static class Settings {
+		static ISettings CurrentSettings => Plugin.Settings.CrossSettings.Current;
+
+		public static string TextFilePath {
+			get => CurrentSettings.GetValueOrDefault (nameof (TextFilePath), "");
+			set => CurrentSettings.AddOrUpdateValue (nameof (TextFilePath), value);
+		}
+
+
+		public static string Profile {
+			get => CurrentSettings.GetValueOrDefault (nameof (Profile), "Default");
+			set => CurrentSettings.AddOrUpdateValue (nameof (Profile), value);
+		}
+
+
+		public static DateTime LastTweetTime {
+			get => CurrentSettings.GetValueOrDefault (nameof (LastTweetTime), DateTime.MinValue);
+			set => CurrentSettings.AddOrUpdateValue (nameof (LastTweetTime), value);
+		}
+
+	}
+}
